b1b2ce3ff514a0b6ba5d4a2392d45d553017674b
Martin Drozdík <drozdik.m@seznam.cz>
2018-09-09 15:41:02 +0200

    typings

---
+++ dist/testing.d.ts
@@ -0,0 +1,43 @@
+/**
+ * Class for simple testing
+ * @example testing.Assert(1 + 2 === 4) -> this will write an error into the console
+ * @author Martin Drozdík <info@bonsai-development.cz> (https://bonsai-development.cz)
+ */
+export declare class Testing {
+    private testName;
+    private assertCount;
+    private errorCount;
+    /**
+     * Create new test
+     * @param testName Test name (optional)
+     */
+    constructor(testName?: string);
+    /**
+     * Set the testing name
+     * @param testName Name of the test
+     */
+    SetTestName(testName: string): void;
+    /**
+     * Returns current test name
+     * @returns Current test name
+     */
+    GetTestName(): string;
+    /**
+     * Testing method. If input is false, error is called.
+     * @param input Testing sample (boolean)
+     */
+    Assert(input: boolean): void;
+    /**
+     * Testing method. If input is false, error is called.
+     * @param input Testing sample (boolean)
+     */
+    static Assert(input: boolean): void;
+    /**
+     * Prints "start statement"
+     */
+    StartTestingLog(): void;
+    /**
+     * Prints "end statement" with error count
+     */
+    EndTestingLog(): void;
+}
--- dist/testing.js
+++ dist/testing.js
@@ -1,18 +1,51 @@
 exports.__esModule = true;
-var Testing = (function () {
+/**
+ * Class for simple testing
+ * @example testing.Assert(1 + 2 === 4) -> this will write an error into the console
+ * @author Martin Drozdík <info@bonsai-development.cz> (https://bonsai-development.cz)
+ */
+var Testing = /** @class */ (function () {
+    //--------------------------------------------------
+    //----------CONSTRUCTOR-----------------------------
+    //--------------------------------------------------
+    /**
+     * Create new test
+     * @param testName Test name (optional)
+     */
     function Testing(testName) {
         if (testName === void 0) { testName = ""; }
+        //--------------------------------------------------
+        //----------ATTRIBUTES------------------------------
+        //--------------------------------------------------
         this.testName = "";
         this.assertCount = 0;
         this.errorCount = 0;
         this.testName = testName;
     }
+    //--------------------------------------------------
+    //----------TEST NAME-------------------------------
+    //--------------------------------------------------
+    /**
+     * Set the testing name
+     * @param testName Name of the test
+     */
     Testing.prototype.SetTestName = function (testName) {
         this.testName = testName;
     };
+    /**
+     * Returns current test name
+     * @returns Current test name
+     */
     Testing.prototype.GetTestName = function () {
         return this.testName;
     };
+    //--------------------------------------------------
+    //----------ASSERT----------------------------------
+    //--------------------------------------------------
+    /**
+     * Testing method. If input is false, error is called.
+     * @param input Testing sample (boolean)
+     */
     Testing.prototype.Assert = function (input) {
         if (!input) {
             console.error("Assert #" + this.assertCount + " failed");
@@ -20,13 +53,26 @@ var Testing = (function () {
         }
         this.assertCount++;
     };
+    /**
+     * Testing method. If input is false, error is called.
+     * @param input Testing sample (boolean)
+     */
     Testing.Assert = function (input) {
         if (!input)
             console.error("Assert failed");
     };
+    //--------------------------------------------------
+    //----------CONSOLE---------------------------------
+    //--------------------------------------------------
+    /**
+     * Prints "start statement"
+     */
     Testing.prototype.StartTestingLog = function () {
         console.log("-----" + this.testName + (this.testName === "" ? "" : " ") + "test start-----");
     };
+    /**
+     * Prints "end statement" with error count
+     */
     Testing.prototype.EndTestingLog = function () {
         console.log("------" + this.testName + (this.testName === "" ? "" : " ") + "test end------");
         console.log(" > " + (this.errorCount == 0 ? "No errors found" : this.errorCount + " error" + (this.errorCount > 1 ? "s" : "") + " found"));
